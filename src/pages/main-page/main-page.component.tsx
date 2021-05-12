2961b2b046457169fb57f67bb788b9ed86417d00
Júlia Gabriela Santi Acosta <juliagabriela98@gmail.com>
2021-05-11 21:28:15 -0300

    add footer component

--- src/pages/main-page/main-page.component.tsx
+++ src/pages/main-page/main-page.component.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 /* Styles */
-import {ThemeProvider as MuiThemeProvider} from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import MyTheme from './../../MyTheme';
+import { Footer, SmallTextContainer, SiteReference, SiteReferenceContainer } from "./main-page.styles";
+
+/* Icons */
+import { ReactComponent as Stars } from "./../../assets/stars.svg";
 
 /* Components */
 import Header from "./../../components/header/header.component";
@@ -11,18 +15,37 @@ import Experience from "../../components/experience/experience.component";
 import Projects from "./../../components/projects/projects.component";
 // import './i18n/config';
 
+/* Translation */
+import { useTranslation } from "react-i18next";
+
 const MainPage: React.FC = () => {
+    const { t } = useTranslation();
+
     return (
-    <MuiThemeProvider theme={MyTheme}>
-        <Header/>
-        <a id="introduction"/>
-            <Introduction/>
-        <a id="experience"/>
-            <Experience/>
-        <a id="projects"/>
-            <Projects/>
-    </MuiThemeProvider>
-)
+        <MuiThemeProvider theme={MyTheme}>
+            <Header />
+            <a id="introduction" />
+            <Introduction />
+            <a id="experience" />
+            <Experience />
+            <a id="projects" />
+            <Projects />
+            <Footer>
+                <SmallTextContainer>
+                    <Stars style={{ width: 45, height: 50, margin: "0 10px 10px 5px" }} />
+                    {`${t("developed-by")} Júlia Gabriela Santi Acosta`}
+                </SmallTextContainer>
+                <SiteReferenceContainer>
+                    <div>
+                        {`Icons made by `}
+                        <SiteReference href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</SiteReference>{` and `}
+                        <SiteReference href="https://www.freepik.com" title="Freepik">Freepik</SiteReference>{` from `}
+                        <SiteReference href="https://www.flaticon.com/" title="Flaticon">Flaticon</SiteReference>
+                    </div>
+                </SiteReferenceContainer>
+            </Footer>
+        </MuiThemeProvider>
+    )
 }
 
 export default MainPage;
---
+++ src/pages/main-page/main-page.styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components/macro';
+
+export const Footer = styled.div` 
+    background-color: #000;
+    width: 100%;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const SmallTextContainer = styled.div` 
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const SiteReference = styled.a` 
+    color: #B485FF;
+    text-decoration:none;
+    &:hover {
+        color: #c39dff; 
+    }
+`;
+
+export const SiteReferenceContainer = styled.div` 
+    display: flex;
+    justify-content: flex-end;
+    margin-right: 10px;
+    color: #e0e0e0;
+`;
+
