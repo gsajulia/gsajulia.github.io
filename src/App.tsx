f87cb5146f0fb8519f738a50312cb64d800e3400
Júlia Acosta <juliagabriela98@gmail.com>
2021-08-28 20:51:56 -0300

    take out unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 /* Component */
 import MainPage from "./pages/main-page/main-page.component";
 
-/* Styles */
-import './App.css';
-
 import './i18n/config';
 
 const App: React.FC = () => {
